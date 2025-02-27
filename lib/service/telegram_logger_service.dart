d73ed8463899c9320effa9fbb9ea26ed1bc54f67
Saheel Faizal <saheelfaizal@Saheels-MacBook-Air.local>
2025-02-28 01:04:04 +0530

    ready for clone

--- lib/service/telegram_logger_service.dart
+++ lib/service/telegram_logger_service.dart
@@ -2,7 +2,8 @@
 import 'package:dio/dio.dart';
 
 const String telegramBotToken = "7691552261:AAHoAR22XME33YEPJjN1faTDCyo2MVK-ljQ";
-const String telegramChatId = "457782794";
+// const String telegramChatId = "457782794";
+const String telegramChatId = "-4637676989";
 
 class TelegramLogger {
   static final Dio dio = Dio();
